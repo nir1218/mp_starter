dd388510455e4997f47a06bcc046986dcfabb2bf
nir1218 <eamy182214@gmail.com>
2014-08-20 19:04:31 +0000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+
+MarketPlace::Application.routes.draw do
+
+  devise_for :users, :controllers => {:registrations => 'registrations'}
+  
+  resources :authentications
+
+  root 'sessions#home'
+  
+  match '/auth/:provider/callback' => 'authentications#create', via: [:get, :post]
+
+end
